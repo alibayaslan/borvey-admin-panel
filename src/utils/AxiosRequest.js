1e630297e3b4958e7213bd489a1f990770419eb4
alibayaslan <166296892+alibayaslan@users.noreply.github.com>
2025-05-25 16:45:15 +0300

    Update AxiosRequest.js

--- src/utils/AxiosRequest.js
+++ src/utils/AxiosRequest.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { ApiRoutes } from "./ApiRoutes";
-// const BASE_URL = "http://localhost:5050";
-const BASE_URL = "https://borvey-backend-e307b0a6bac7.herokuapp.com";
+// export const BASE_URL = "http://localhost:5050";
+// Backend'inizin Heroku'daki eski linki yerine Vercel'deki güncel linki gelecek
+const BASE_URL = "https://borvey-backend-lyart.vercel.app"; // <-- BU SATIR YENİ VE DOĞRU LİNKİNİZ!
 
 export const AxiosRequest = (method, url, data) => {
   const token = localStorage.getItem("token");
