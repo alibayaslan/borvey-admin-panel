3545a6d27754554d86008d58d9d6faa9a205c4eb
alibayaslan <166296892+alibayaslan@users.noreply.github.com>
2025-05-25 17:16:33 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,22 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom"; 
 import App from "./App";
 import { Provider } from "react-redux";
 import configureStore from "./configureStore";
 import { SnackbarProvider } from "notistack";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// const root = ReactDOM.createRoot(document.getElementById("root")); 
 
 export const store = configureStore();
 
 export const state = store.getState();
-root.render(
+
+// root.render yerine ReactDOM.render kullanıldı
+ReactDOM.render(
   <Provider store={store}>
     <SnackbarProvider maxSnack={3}>
       <App />
     </SnackbarProvider>
-  </Provider>
+  </Provider>,
+  document.getElementById("root") 
 );
